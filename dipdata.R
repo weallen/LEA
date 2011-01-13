8f56b9caf74c873aa3a3887866e32485ebd4d78c
Will Allen <we.allen@gmail.com>
2011-01-13 15:07:39 -0500

    Fixed load.DipData so loads from cache if available

--- dipdata.R
+++ dipdata.R
@@ -4,6 +4,7 @@ library(biganalytics)
 library(multicore)
 library(foreach)
 library(doMC)
+#library(qvalue)
 
 #options(bigmemory.typecast, warning=FALSE)
 options(bigmemory.allow.dimnames=TRUE)
@@ -36,31 +37,54 @@ save.DipData <- function(dip.data) {
   write.big.matrix(dip.data.col, paste(data.path, paste(colname, "txt", sep="."), sep="/"))
 }
 
-.readDipData <- function(data.path, colname, dtype) {
+.readDipData <- function(colname, dtype) {
   col.path <- paste(colname, "txt", sep=".")
   col.bin.path <- paste(colname, "bin", sep=".")
-  return(read.big.matrix(paste(data.path, col.path, sep="/"), header=TRUE, 
+  return(read.big.matrix(col.path, header=TRUE, 
                          type=dtype, backingfile=col.bin.path,
-                         backingpath=data.path,
+                         backingpath=".",
                          descriptorfile=paste(colname, "desc",sep=".")))
 }
 
 load.DipData <- function(dip.data.name) {
+  .doLoad <- function() {
+    obj <- DipData(dip.data.name)
+    if (!file.exists("genome_data.bin") ||
+        !file.exists("genome_data.desc")) {
+      obj$genome.data <- .readDipData("genome_data", "double")    
+    }
+    else {
+      obj$genome.data <- attach.big.matrix(dget("genome_data.desc"))
+    }
+    chr.data <- read.table(dip.data.name, header=TRUE, sep=",")
+    obj$chr.names <- chr.data$chr.names
+    obj$chr.lengths <- chr.data$chr.lengths
+    obj$bin.size <- chr.data$bin.size
+    return(obj)
+  }
+  # kind of a kludge but w/e
+  old.wd <- getwd()
   data.path <- paste(dip.data.name, "dipdata", sep=".")
   if (!file.exists(data.path)) {
-    stop("Error: Dipdata file does not exist")
+    stop("Dipdata file does not exist")
   }
-  obj <- DipData(dip.data.name)
-  obj$genome.data <- .readDipData(data.path, "genome_data", "double")
-  chr.data <- read.table(paste(data.path, dip.data.name, sep="/"), header=TRUE)
-  obj$chr.names <- chr.data$chr.names
-  obj$chr.lengths <- chr.data$chr.lengths
-  obj$bin.size <- chr.data$bin.size
+  setwd(data.path) 
+  tryCatch(obj <- .doLoad(), finally=setwd(old.wd))
   return(obj)
 }
 
-foldChange.DipData <- function(dd1, dd2) {
-  
+foldChange.DipData <- function(dd1, dd2, type="raw") {
+  require(gtools)
+  if (type != "raw" && type != "norm") {
+    stop("Type must be raw or norm")
+  }
+  chr.idx <- bigsplit(dd$genome.data, 'chr')
+  fc <- foreach(chr=chr.idx, .combine=c) %dopar% {
+    sapply(chr, function(i) {
+      return(foldchange(dd1$genome.data[i, type], dd2$genome.data[i, type]))
+    })
+  }
+  return(fc)
 }
 
 diffEnrichment.DipData <- function(dd1, dd2) {
@@ -77,13 +101,10 @@ diffEnrichment.DipData <- function(dd1, dd2) {
   }
   return(p.val)
 }
-  
-#fisherTestResults = apply(frequencyTable,1,function(X)
-#  { contingencyTable = matrix(c(X["sampleReads"],sampleTotal-X["sampleReads"],
-#                                X["otherSampleReads"],otherSampleTotal-X["otherSampleReads"]),nrow
-#                                                                   =2,ncol=2); fast.fisher(contingencyTable) })
-#pValueVsOthers = pvalues = unlist(lapply(fisherTestResults, function(X) { X[["p.value"]] }))
-#qvalueObject = try(qvalue(signif(pvalues,8))) # "qvalue fails if values are marginally higher than 1, also revert to Bonferroni if FDR estimation fails due to low sample size and odd p-value distri
-  
-#  fisher.fast
+
+
+# Usually you'd want to do multiple hypothesis adjustment before doing this
+saveDiffEnriched.DipData <- function(fname, dd, p.vals, cutoff=0.05) {
+  # TODO implement
 }
+
